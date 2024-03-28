bb9fdf36e69f9b765e8a0c3007381c9f89696fbf
Shiraz Ali <shiraz998ali@gmail.com>
2024-03-28 17:15:15 +0500

    done

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -24,8 +24,8 @@ var swiper = new Swiper(".card-slider", {
         //spaceBetween: 40,
     },
     1024: {
-        slidesPerView: 5,
-        //spaceBetween: 50,
+        slidesPerView: 4
+        //spaceBetween: 20,
     },
     1300: {
         slidesPerView: 6,
@@ -42,7 +42,7 @@ var swiper = new Swiper(".slider-grids", {
     grid: {
       rows: 1,
     },
-    spaceBetween: 30,
+   // spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -51,5 +51,38 @@ var swiper = new Swiper(".slider-grids", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      320: {
+          slidesPerView: 1,
+          //spaceBetween: 20,
+      },
+      425: {
+          slidesPerView: 3,
+          //spaceBetween: 20,
+      },
+      768: {
+          slidesPerView: 4,
+          //spaceBetween: 40,
+      },
+      1024: {
+          slidesPerView: 4
+          //spaceBetween: 20,
+      },
+      1300: {
+          slidesPerView: 4,
+          spaceBetween: 140,
+      },
+      },
 });
 /// Close Swiper Gallery ///
+
+var swiper = new Swiper(".drop-slider", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
